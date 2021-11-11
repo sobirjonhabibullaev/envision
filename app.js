2f265e58660ebe26f5768f7c06289605336a6787
Sobirjon Habibullaev <onlinecourses1999@gmail.com>
2021-11-11 23:21:47 +0900

    one page

--- app.js
+++ app.js
@@ -76,20 +76,3 @@ function setup() {
      // Classifiy again!
      classifyVideo();
  }
-const title = document.getElementById('vidi');
-const text = "Hello Guys, Welcome to my website";
-
-const animateText = (text, element, intervalDuration) => {
-  let count = 0;
-  const interval = setInterval(() => {
-    const part = text.substring(0, ++count);
-    requestAnimationFrame(() => {
-      element.textContent = part;
-    });
-    if (count === text.length) {
-      clearInterval(interval);
-    }
-  }, intervalDuration);
-};
-
-animateText(text, title, 100);
