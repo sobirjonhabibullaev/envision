14415ac1c24f178791582dc569141a3bb720ec93
Sobirjon Habibullaev <onlinecourses1999@gmail.com>
2021-11-12 19:50:57 +0900

    Ready testing

--- app.js
+++ app.js
@@ -1,105 +1,101 @@
 window.onload = function () {
   let vid = document.getElementById('vid');
-  vid.duration = 1;
-} 
- 
-
-   
-  // Classifier Variable
-   let classifier;
-   // Model URL
-   let imageModelURL =
-     "https://teachablemachine.withgoogle.com/models/lIvux57Wc/";
-
-   // Video
-   let video;
-   let flippedVideo;
-   // To store the classificationpip install streamlit
-   let label = "";
-   let img;
-   // Load the model first
-   function preload() {
-     classifier = ml5.imageClassifier(imageModelURL + "model.json");
-     img = loadImage("data/logo.png");
-   }
-
-   function setup() {
-     createCanvas(400, 660);
-     // Create the video
-     //video = createCapture(VIDEO);
-
-     var constraints = {
-       audio: false,
-       video: {
-         facingMode: "environment",
-       },
-     };
-     video = createCapture(constraints);
-     video.size(400, 660);
-     video.hide();
-
-     flippedVideo = ml5.flipImage(video);
-     // Start classifying
-     classifyVideo();
-   }
-
-   function draw() {
-     background(0);
-     // Draw the video
-     image(video, 0, 0);
-
-     // Draw the label
-     fill(255);
-     textSize(30);
-     textAlign(CENTER);
-     text(label, width / 2, height - 60);
-     image(img, 10, height - 100, 100, 52);
-   }
-
-   // Get a prediction for the current video frame
-   function classifyVideo() {
-     flippedVideo = ml5.flipImage(video);
-     classifier.classify(flippedVideo, gotResult);
-   }
-
-   // When we get a result
-   function gotResult(error, results) {
-     // If there is an error
-     if (error) {
-       console.error(error);
-       return;
-     }
-     // The results are in an array ordered by confidence.
-     // console.log(results[0]);
-     if (results[0].confidence > 0.9) {
-       label = results[0].label;
-
-       console.log(label);
-       add_url(label)
-     } else {
-       label = "...";
-     }
-     flippedVideo.remove();
-     // Classifiy again!
-     classifyVideo();
-   }
-
- 
+};
+// Classifier Variable
+let classifier;
+// Model URL
+let imageModelURL = 'https://teachablemachine.withgoogle.com/models/lIvux57Wc/';
+let link = '';
+let label = '';
+// Video
+let video;
+let flippedVideo;
+// To store the classificationpip install streamlit
+let img;
 
 
- 
- function add_url(n) {
-   if (n == "Astronomy") {
-     vid.setAttribute("src", "Astrology.ogg");
-   } else if (n == "Biology") {
-     vid.setAttribute("src", "Biology.mp4");
-   } else if (n == "Chemistry") {
-     vid.setAttribute("src", "Chemistry.mp4");
-   } else if (n == "Medicine") {
-     vid.setAttribute("src", "Medicine.mp4");
-   } else if (n == "Physics") {
-     vid.setAttribute("src", "Astrology.ogg");
-   }else{
-     vid.setAttribute("src", "#");
-   }
- }
+// Load the model first
+function preload() {
+  classifier = ml5.imageClassifier(imageModelURL + 'model.json');
+  img = loadImage('data/logo.png');
+}
+
+function setup() {
+  createCanvas(400, 660);
+  // Create the video
+  // video = createCapture(VIDEO);
+
+  var constraints = {
+    audio: false,
+    video: {
+      facingMode: 'environment',
+    }
+  };
+  video = createCapture(constraints);
+  video.size(400, 660);
+  video.hide();
+  flippedVideo = ml5.flipImage(video);
+
+  // Start classifying
+  classifyVideo();
+}
+
+function draw() {
+  background(0);
+
+  // Draw the video
+  image(video, 0, 0);
+
+  // Draw the label
+  fill(255);
+  textSize(30);
+  textAlign(CENTER);
+  text(label, width / 2, height - 60);
+  image(img, 10, height - 100, 100, 52);
+  
+}
+
+// Get a prediction for the current video frame
+function classifyVideo() {
+  flippedVideo = ml5.flipImage(video);
+  classifier.classify(flippedVideo, gotResult);
+
+}
+
+// When we get a result
+function gotResult(error, results) {
+  // If there is an error
+  if (error) {
+    console.error(error);
+    return;
+  }
+  // The results are in an array ordered by confidence.
+
+  if (results[0].confidence > 0.95) {
+
+    console.log(label);
+    label = results[0].label;
+    
+    if (label == "Astronomy") {
+      vid.src = "/videos/Astrology.mov";
+    } else if (label == "Biology") {
+      vid.src = "/videos/Biology.mov";
+    } else if (label == "Chemistry") {
+      vid.src = "/videos/Chemistry.mov";
+    } else if (label == "Medicine") {
+      vid.src = "/videos/Medicine.mov";
+    } else if (label == "Physics") {
+      vid.src = "/videos/Physics.mov";
+    } else {
+      vid.src = "#";
+    }
+    
+  } else {
+    label = '...';
+  }
+  flippedVideo.remove();
+  // Classifiy again!
+  classifyVideo();
+  // return add_url(label);
+}
+
