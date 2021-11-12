993e52e601f1c8fcd2b6aa939b5323da4a744d84
Sobirjon Habibullaev <onlinecourses1999@gmail.com>
2021-11-12 19:36:17 +0900

    ready to use

--- 1.js
+++ 1.js
@@ -1,45 +1,41 @@
 window.onload = function () {
   let vid = document.getElementById('vid');
-
-  console.log('start');
-  setTimeout(() => {
-    console.log('this is the first message');
-  }, 5000);
-  console.log('end');
 };
 // Classifier Variable
 let classifier;
 // Model URL
 let imageModelURL = 'https://teachablemachine.withgoogle.com/models/lIvux57Wc/';
-
+let link = '';
 let label = '';
 // Video
 let video;
 let flippedVideo;
 // To store the classificationpip install streamlit
 let img;
+// let vid;
 
 // Load the model first
 function preload() {
   classifier = ml5.imageClassifier(imageModelURL + 'model.json');
   img = loadImage('data/logo.png');
+  // vid = loadImage('Chemistry.gif');
+  // vid.id = 'cccc';
 }
 
 function setup() {
   createCanvas(400, 660);
   // Create the video
-  //video = createCapture(VIDEO);
+  // video = createCapture(VIDEO);
 
   var constraints = {
     audio: false,
     video: {
       facingMode: 'environment',
-    },
+    }
   };
   video = createCapture(constraints);
   video.size(400, 660);
   video.hide();
-
   flippedVideo = ml5.flipImage(video);
   // Start classifying
   classifyVideo();
@@ -49,25 +45,22 @@ function draw() {
   background(0);
   // Draw the video
   image(video, 0, 0);
-
+  // image(vid, 0, 250);
   // Draw the label
+  // image(vid, 0, 300, 400, 250);
   fill(255);
   textSize(30);
   textAlign(CENTER);
   text(label, width / 2, height - 60);
   image(img, 10, height - 100, 100, 52);
+  
 }
 
 // Get a prediction for the current video frame
 function classifyVideo() {
   flippedVideo = ml5.flipImage(video);
-  setTimeout(
-    () => {
-      classifier.classify(flippedVideo, gotResult);
-      console.log(vid.duration);
-    },
-    int(vid.duration * 1000) ? NaN : 0
-  );
+  classifier.classify(flippedVideo, gotResult);
+
 }
 
 // When we get a result
@@ -79,41 +72,38 @@ function gotResult(error, results) {
   }
   // The results are in an array ordered by confidence.
   // console.log(results[0]);
-  if (results[0].confidence > 0.9) {
-    label = results[0].label;
-    // output(label);
+  if (results[0].confidence > 0.95) {
+        // output(label);
     console.log(label);
-
-    if (label == 'Chemistry') {
-      vid.src = 'Astrology.ogg';
+    label = results[0].label;
+    
+    if (label == "Astronomy") {
+      vid.src = "/videos/Astrology.mov";
+    } else if (label == "Biology") {
+      vid.src = "/videos/Biology.mov";
+    } else if (label == "Chemistry") {
+      vid.src = "/videos/Chemistry.mov";
+    } else if (label == "Medicine") {
+      vid.src = "/videos/Medicine.mov";
+    } else if (label == "Physics") {
+      vid.src = "Astrology.ogg";
+    } else {
+      vid.src = "#";
     }
-    if (label == 'Biology') {
-      vid.src = 'https://www.w3schools.com/html/mov_bbb.mp4';
-    }
+    
   } else {
     label = '...';
   }
   flippedVideo.remove();
   // Classifiy again!
   classifyVideo();
+  // return add_url(label);
 }
-console.log(label);
 
-function add_url(n) {
-  if (n == 'Astronomy') {
-    v_id.setAttribute('src', 'Astrology.ogg');
-  } else if (n == 'Biology') {
-    v_id.setAttribute('src', 'Astrology.ogg');
-  } else if (n == 'Biology') {
-    v_id.setAttribute('src', 'Astrology.ogg');
-  } else if (n == 'Biology') {
-    v_id.setAttribute('src', 'Astrology.ogg');
-  } else if (n == 'Biology') {
-    v_id.setAttribute('src', 'Astrology.ogg');
-  } else {
-    v_id.setAttribute('src', '#');
-  }
-}
+
+ function add_url(l) {
+   
+ }
 
 // function output(l) {
 
